a81252dec32df8bcff912e62ba35241192588337
mazzzzzzzneva <maznevaofficial@gmail.com>
2024-02-21 12:55:28 +0300

    f

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,42 +5,21 @@ import {
 
 const brainPrime = () => {
   const game = 'prime';
-
   const name = startGame('Answer "yes" if given number is prime. Otherwise answer "no".');
-  bodyGame(name,answer)
-  const number = getNumber();
-  showQuestion(game, number);
-  const answer = readlineSync.question('Your answer: ');
-  const rigthAnswer = isPrime(number); 
-
-  const message = checkAnswer = (name, answer, isPrime());
-
-  return message;
-  }
-
-  export const bodyGame = (name, body) => {
-    let i = 0;
-    while (i < 3) {
-      const message = body();
-      if (message === 'Correct!') {
-        i += 1;
-      } else {
-        break;
+  bodyGame(name, () => {
+    const number = getNumber();
+    showQuestion(game, number);
+    const answer = readlineSync.question('Your answer: ');
+    const message = checkAnswer(name, answer, () => {
+      let correctAnswer = number !== 1;
+      for (let i = 2; i < number; i += 1) {
+        correctAnswer *= number % i !== 0;
       }
-    }
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  };
-  
-const isPrime = (number) => {
-    for (let i = 2; i < number; i += 1) {
-      return 'no';
-    }
-    return 'yes';
-  };
-
-  showMessage(message);
-
+      return correctAnswer ? 'yes' : 'no';
+    });
+    showMessage(message);
+    return message;
+  });
+};
 
 export default brainPrime;
